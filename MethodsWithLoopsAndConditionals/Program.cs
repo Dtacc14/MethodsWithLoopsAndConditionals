676e797e94aabf0327f2c757575811619e6ba995
David Taccetta <taccetta14@gmail.com>
2022-06-06 16:44:48 -0400

    Made fixes to TryParse statements

--- MethodsWithLoopsAndConditionals/Program.cs
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -87,7 +87,7 @@ namespace MethodsWithLoopsAndConditionals
             numbersByThree();
             Console.WriteLine();
 
-            equal(0,0);
+            equal(0, 0);
             Console.WriteLine();
 
             evenOrOdd();
@@ -116,33 +116,51 @@ namespace MethodsWithLoopsAndConditionals
 
         static void numbersByThree()
         {
-            for (int i = 1; i < 333 ; i++)
+            for (int i = 0; i < 1000 ; i += 3)
             {
-                Console.WriteLine(i * 3);
+                Console.WriteLine(i);
             } 
         }
-        static void equal(int x ,int y)
+        static void equal(int x, int y)
         {
-            var userGuess = x;
-            var userGuessTwo = y;
-            Console.WriteLine("Pick a number");
-            var works = int.TryParse(Console.ReadLine(), out userGuess);
-            Console.WriteLine("Pick a second number");
-            var worksTwo = int.TryParse(Console.ReadLine(), out userGuessTwo);
+            int userGuess = x;
+            int userGuessTwo = y;
+            do
+            {
+                Console.WriteLine("Pick a number");
+                var works = int.TryParse(Console.ReadLine(), out userGuess);
+            }
+            while (userGuess == 0);
+            do
+            {
+                Console.WriteLine("Pick a second number");
+                var worksTwo = int.TryParse(Console.ReadLine(), out userGuessTwo);
+            }
+            while(userGuessTwo == 0);
+
             if (userGuess > userGuessTwo)
             {
                 Console.WriteLine($"{userGuess} is greater than {userGuessTwo}"); 
             }
-            else
+            else if (userGuess < userGuessTwo)
             {
                 Console.WriteLine($"{userGuessTwo} is greater than {userGuess}");
+            }
+            else
+            {
+                Console.WriteLine($"{userGuess} is even with {userGuessTwo}.");
             }
         }
         static void evenOrOdd()
         {
             int userGuess;
-            Console.WriteLine("Pick a number, any number!");
-            var works = int.TryParse(Console.ReadLine(), out userGuess);
+            do
+            {
+                Console.WriteLine("Pick a number, any number!");
+                var works = int.TryParse(Console.ReadLine(), out userGuess);
+            }
+            while (userGuess == 0);
+
             if (userGuess%2==0)
             {
                 Console.WriteLine($"{userGuess} is an even number.");
@@ -155,8 +173,13 @@ namespace MethodsWithLoopsAndConditionals
         static void positiveOrNegative()
         {
             int userGuess;
-            Console.WriteLine("Pick a number to decide if it is positive or negative.");
-            var works = int.TryParse(Console.ReadLine(), out userGuess);
+            do
+            {
+                Console.WriteLine("Pick a number to decide if it is positive or negative.");
+                var works = int.TryParse(Console.ReadLine(), out userGuess);
+            }
+            while (userGuess == 0);
+
             if (userGuess > 0)
             {
                 Console.WriteLine($"{userGuess} is a positive number.");
@@ -170,23 +193,32 @@ namespace MethodsWithLoopsAndConditionals
         static void canYouVote()
         {
             int userAge;
-            Console.Write("Are you old enough to vote? Input age: ");
-            var works = int.TryParse(Console.ReadLine(), out userAge);
+            do
+            {
+                Console.Write("Are you old enough to vote? Input age: ");
+                var works = int.TryParse(Console.ReadLine(), out userAge);
+            }
+            while (userAge == 0);
+
             if (userAge >= 18)
             {
-                Console.WriteLine("You are old enough to vote.");
+                Console.WriteLine("Congratulation! You're old enough to vote.");
             }
             else
             {
-                Console.WriteLine("You are too young to vote.");
+                Console.WriteLine("We're sorry, you're too young to vote.");
             }
         }
 
         static void inRange()
         {
             int userInput;
-            Console.WriteLine("Is your number between -10 and 10? Input number: ");
-            var works = int.TryParse(Console.ReadLine(), out userInput);
+            do
+            {
+                Console.WriteLine("Is your number between -10 and 10? Input number: ");
+                var works = int.TryParse(Console.ReadLine(), out userInput);
+            }
+            while (userInput == 0);
             if (userInput >= -10 && userInput <= 10)
             {
                 Console.WriteLine($"Yes! {userInput} is in range!");
@@ -200,8 +232,12 @@ namespace MethodsWithLoopsAndConditionals
         static void multiplication()
         {
             int multiplicationNumber;
-            Console.WriteLine("What number would you like to see the multiplication table for?");
-            var works = int.TryParse(Console.ReadLine(), out multiplicationNumber);
+            do
+            {
+                Console.WriteLine("What number would you like to see the multiplication table for?");
+                var works = int.TryParse(Console.ReadLine(), out multiplicationNumber);
+            }
+            while (multiplicationNumber == 0);
             Console.WriteLine($"{multiplicationNumber * 1}, {multiplicationNumber * 2}, {multiplicationNumber * 3}, {multiplicationNumber * 4}, {multiplicationNumber * 5}, {multiplicationNumber * 6}, " +
                 $"{multiplicationNumber * 7}, {multiplicationNumber * 8}, {multiplicationNumber * 9}, {multiplicationNumber * 10}, {multiplicationNumber * 11}, {multiplicationNumber * 12}");
         }
